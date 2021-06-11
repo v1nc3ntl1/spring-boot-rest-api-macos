1cae2cb3959a41c84bb1d131524991f486a4f617
vincentli <vli@moneylion.com>
2021-06-11 15:30:30 +0800

    remove product controller and underlying function

--- src/main/java/com/example/moneylion/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.moneylion.repository;
-
-import com.example.moneylion.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-    public Product findByTitle(String title);
-
-}
